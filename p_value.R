dae65900954cc920fe482831eaab3d3d5f586a75
Neal Yu <nealxun@gmail.com>
2020-05-25 16:36:33 -0700

    add p-value section

---
+++ p_value.R
@@ -0,0 +1,19 @@
+# objective: show a scenaio of deceive p-value
+# reference: https://www4.stat.ncsu.edu/~stefanski/NSF_Supported/Hidden_Images/orly_owl_files/orly_owl_Lin_4p_5_flat.txt
+
+# deceive p-value
+dat <- read.table("orly_owl_Lin_4p_5_flat.txt")
+pairs(dat)
+summary(lm(V1 ~ . -1, data = dat))$coef
+fit <- lm(V1 ~ . - 1, data = dat)
+summary(fit)
+plot(predict(fit), resid(fit), pch = '.')
+
+# influential point
+n <- 100
+x <- c(10, rnorm(n)); y <- c(10, c(rnorm(n)))
+plot(x, y, frame = FALSE, cex = 2, pch = 21, bg = "lightblue", col = "black")
+abline(lm(y ~ x))
+fit <- lm(y ~ x)
+summary(fit)
+round(dfbetas(fit)[1 : 10, 2], 3)
--- three_door_sim.R
+++ three_door_sim.R
@@ -16,11 +16,12 @@ monty_hall_game <- function(door_num = 3, switch = FALSE) {
                 # cross out doors
                 door_not_selected <- doors[!doors %in% c(initial_selection)]
                 
+                if (door_price == initial_selection) {
+                        door_cross_out <- base::sample(doors[!doors %in% c(door_price, initial_selection)], door_num - 2)
+                } else {
+                        door_cross_out <- doors[!doors %in% c(door_price, initial_selection)]
+                }
                 
-                
-                door_cross_out <- ifelse(door_price == initial_selection,
-                                         base::sample(doors[!doors %in% c(door_price, initial_selection)], door_num - 2),
-                                         doors[!doors %in% c(door_price, initial_selection)])
                 second_selection <- door_not_selected[!door_not_selected %in% door_cross_out]
                 result <- ifelse(second_selection == door_price, "WIN", "NOT WIN")
         } else {
@@ -48,7 +49,7 @@ monty_hall_sim <- function(door_num = 3, game = 100, switch = FALSE) {
 }
 
 # run simulation
-DOOR_NUM <- 5
+DOOR_NUM <- 100
 GAME <- 10000
 df_sim_not_switch <- monty_hall_sim(DOOR_NUM, game = GAME, switch = FALSE)
 df_sim_switch <- monty_hall_sim(DOOR_NUM, game = GAME, switch = TRUE)
@@ -56,11 +57,13 @@ df_sim_switch <- monty_hall_sim(DOOR_NUM, game = GAME, switch = TRUE)
 # visual check
 df <- bind_rows(df_sim_not_switch, df_sim_switch) %>%
         group_by(switch, result) %>%
-        summarise(count = n())
+        summarise(count = n()) %>%
+        ungroup() %>%
+        mutate(switch = ifelse(switch == TRUE, "Switch", "Not Switch"))
 ggplot(df, aes(x = switch, y = count, fill = result)) +
         geom_bar(stat = "identity", position = "dodge") +
-        geom_text(aes(label = count), position =position_dodge(width=0.9), vjust=-0.25) +
-        labs(title = "Month Hall Problem Simulation Result", 
+        geom_text(aes(label = count), position = position_dodge(width=0.9), vjust=-0.25) +
+        labs(x = "Strategy", y = "Game Count", title = "Month Hall Problem Simulation Result", 
              subtitle = paste0("Suppose there are ", DOOR_NUM, " doors in total and ", 
                                GAME, " games are played"))
 
