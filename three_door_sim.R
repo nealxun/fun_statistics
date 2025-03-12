cc97339b8b2a9e75d9c6437df21a8eccdfcde6a1
Neal Yu <nealxun@gmail.com>
2025-03-11 22:17:11 -0700

    enhance three door sim plot

--- three_door_sim.R
+++ three_door_sim.R
@@ -23,9 +23,9 @@ monty_hall_game <- function(door_num = 3, switch = FALSE) {
                 }
                 
                 second_selection <- door_not_selected[!door_not_selected %in% door_cross_out]
-                result <- ifelse(second_selection == door_price, "WIN", "NOT WIN")
+                result <- ifelse(second_selection == door_price, "WIN", "LOSE")
         } else {
-                result <- ifelse(initial_selection == door_price, "WIN", "NOT WIN")
+                result <- ifelse(initial_selection == door_price, "WIN", "LOSE")
         }
         
         return(result)
@@ -49,7 +49,7 @@ monty_hall_sim <- function(door_num = 3, game = 100, switch = FALSE) {
 }
 
 # run simulation
-DOOR_NUM <- 100
+DOOR_NUM <- 3
 GAME <- 10000
 df_sim_not_switch <- monty_hall_sim(DOOR_NUM, game = GAME, switch = FALSE)
 df_sim_switch <- monty_hall_sim(DOOR_NUM, game = GAME, switch = TRUE)
@@ -59,11 +59,28 @@ df <- bind_rows(df_sim_not_switch, df_sim_switch) %>%
         group_by(switch, result) %>%
         summarise(count = n()) %>%
         ungroup() %>%
-        mutate(switch = ifelse(switch == TRUE, "Switch", "Not Switch"))
-ggplot(df, aes(x = switch, y = count, fill = result)) +
+        mutate(switch = ifelse(switch == TRUE, "Switch", "Not Switch")) |> 
+        mutate(result = fct(result, levels = c("WIN", "LOSE")))
+
+df |> 
+        ggplot(aes(x = switch, y = count, fill = result)) +
         geom_bar(stat = "identity", position = "dodge") +
-        geom_text(aes(label = count), position = position_dodge(width=0.9), vjust=-0.25) +
+        geom_text(aes(label = count), position = position_dodge(width = 0.9), vjust = -0.25) +
         labs(x = "Strategy", y = "Game Count", title = "Month Hall Problem Simulation Result", 
              subtitle = paste0("Suppose there are ", DOOR_NUM, " doors in total and ", 
                                GAME, " games are played"))
+
+df |> 
+        ggplot(aes(x = result, y = count, fill = result)) +
+        geom_bar(stat = "identity", position = "dodge") +
+        geom_text(aes(label = count), position = position_dodge(width = 0.9), vjust = -0.25) +
+        geom_label(aes(label = result), vjust = 2.5, color = "white", size = 5) +
+        facet_wrap(vars(switch), ncol = 1) +
+        scale_fill_manual(values=c("#28A745", 
+                                   "#DC3545"
+                                   )) +
+        labs(x = "", y = "Game Count", title = "If you switch - you get 2/3 chance to win the game", 
+             subtitle = paste0("Suppose there are ", DOOR_NUM, " doors in total and ", 
+                               GAME, " games are played")) +
+        theme(legend.position = "none")
 
